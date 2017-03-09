742c72119a015fef3ed0aceb1bd4f3ff03ef9ebc
GoldingAustin <austinjgolding>
2017-03-08 20:56:56 -0700

    Fixed search view crash

--- app/src/main/java/com/cs246/plantapp/MainActivity.java
+++ app/src/main/java/com/cs246/plantapp/MainActivity.java
@@ -52,12 +52,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
