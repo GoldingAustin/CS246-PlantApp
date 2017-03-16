7ee475a0c995f9cc6b0cdf0683a9b76361725018
GoldingAustin <austinjgolding>
2017-03-16 03:06:22 -0600

    Firebase integrated into addplants, plants list now displays on main page and updates when new plants are added.

--- app/src/main/java/com/cs246/plantapp/MainActivity.java
+++ app/src/main/java/com/cs246/plantapp/MainActivity.java
@@ -3,31 +3,45 @@ package com.cs246.plantapp;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * The type Main activity.
  */
 //Quick change to see merge conflict
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
+    private final ArrayList<PlantsObject> plants = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getFromDatabase();
 
         handleIntent(getIntent());
+
 
         FloatingActionButton myFab = (FloatingActionButton) this.findViewById(R.id.addPlantButton);
         myFab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
     protected void onNewIntent(Intent intent) {
         setIntent(intent);
         handleIntent(intent);
@@ -50,6 +69,12 @@ public class MainActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getFromDatabase();
     }
 
     @Override
@@ -76,5 +101,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void getPlants() {
+        PlantsListAdapter plantsListAdapter = new PlantsListAdapter(getApplicationContext(), plants);
+        Log.d("Results", String.valueOf(plantsListAdapter.getCount()));
+        ListView view = (ListView) findViewById(R.id.listPlantsView);
+        view.setAdapter(plantsListAdapter);
+    }
+
+    private void getFromDatabase() {
+        mDatabase = FirebaseDatabase.getInstance().getReference(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                    plants.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        plants.add(snapshot.getValue(PlantsObject.class));
+                    }
+                    getPlants();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cs246/plantapp/Utilities.java
@@ -0,0 +1,56 @@
+package com.cs246.plantapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Base64;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by austingolding on 3/16/17.
+ */
+
+public class Utilities {
+
+
+    /**
+     * Bit map to string string.
+     *
+     * @param bitmap the bitmap
+     * @return the string
+     */
+/* BitMapToString and StringToBitMap
+     * http://stackoverflow.com/questions/13562429/how-many-ways-to-convert-bitmap-to-string-and-vice-versa
+     */
+    public static String BitMapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] b = baos.toByteArray();
+        String temp = Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+    /**
+     * String to bit map bitmap.
+     *
+     * @param encodedString the encoded string
+     * @return the bitmap
+     */
+    public static Bitmap StringToBitMap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+}
